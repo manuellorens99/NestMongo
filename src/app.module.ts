391026381a2764f21c69e1044ea97d6f6d44a752
Manuel Llorens <pixelartrecompilation@gmail.com>
2025-05-25 13:01:25 +0200

    Static Content Implementation, public html&css files creation and app.module.ts update.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,11 @@
+import { join } from 'path'; //Node
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..','public'),
+    })
+  ],
 })
 export class AppModule {}
